e4432bddd0b5cb7a686ca49989f696bb9c911112
Thiago Rovina <ra124257@uem.br>
2025-07-19 22:16:13 -0300

    Commitando alterações

--- TrabalhoWEB/conectaBanco.php
+++ TrabalhoWEB/conectaBanco.php
@@ -1,17 +1,14 @@
 <?php
 function conectaBanco() {
-
     $db_url = getenv('DATABASE_URL');
 
-  
     if (!$db_url) {
         $db_host = 'localhost';
-        $db_name = 'sebo';     
-        $db_user = 'postgres'; 
-        $db_password = '123';   
+        $db_name = 'sebo';
+        $db_user = 'postgres';
+        $db_password = '123';
         $db_port = '5432';
     } else {
-        
         $url_parts = parse_url($db_url);
         $db_host = $url_parts['host'];
         $db_port = $url_parts['port'];
@@ -20,12 +17,14 @@ function conectaBanco() {
         $db_name = ltrim($url_parts['path'], '/');
     }
 
-    try {
-        $pdo = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        die("Erro na conexão com o banco de dados: " . $e->getMessage());
+    $conn_string = "host=$db_host port=$db_port dbname=$db_name user=$db_user password=$db_password";
+
+    $conn = pg_connect($conn_string);
+
+    if (!$conn) {
+        die("Erro na conexão com o banco de dados: " . pg_last_error());
     }
+
+    return $conn;
 }
 ?>
